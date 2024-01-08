8ce5e09ae47b6123b9c9978883e80dbe68bc9b81
Martin <ilhammartin2@gmail.com>
2024-01-08 23:18:55 +0700

    kasus sederhana

--- kasus_sederhana/index.js
+++ kasus_sederhana/index.js
@@ -1,4 +1,5 @@
 const readline = require('node:readline');
+const fs = require('node:fs');
 
 const { stdin: input, stdout: output } = require('node:process');
 
@@ -9,17 +10,45 @@ const pilih = readline.createInterface({input,output})
 const inputData = ()=>{
     pilih.question("Masukan Nama : ",(nama) => {
         pilih.question("masukan Umur : ", (umur)=>{
+            const datadiri = {
+                nama:nama,
+                umur:umur
+            }
+            const check = fs.readFileSync('api.json','utf-8')
+            const datapribadi = JSON.parse(check)
+            datapribadi.push(datadiri)
+            fs.writeFileSync('api.json',JSON.stringify(datapribadi))
+
             console.log( `nama anda ${nama} umur anda ${umur}`)
+            console.log("data berhasil ditambah")
         pilih.close()
         })
     })
    
 }
 
+const hapusData = ()=>{
+    pilih.question("Masukan Nama : ",(nama) => {
+        pilih.question("masukan Umur : ", (umur)=>{
+            const datadiri = {
+                nama:nama,
+                umur:umur
+            }
+            const check = fs.readFileSync('api.json','utf-8')
+            const datapribadi = JSON.parse(check)
+            datapribadi.pop(datadiri)
+            fs.writeFileSync('api.json',JSON.stringify(datapribadi))
+            console.log('data anda dihapus')
+
+        pilih.close()
+        })
+    })
+}
+
 pilih.question("1.Input Data \n 2. Delete Data 3.Cari Data",(pilihan) => {
     if(pilihan == "1") {
         inputData()
-        console.log("data berhasil ditambah")
-        
+    } else if (pilihan == '2'){
+        hapusData()
     }
 })
