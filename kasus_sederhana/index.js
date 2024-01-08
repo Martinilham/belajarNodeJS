c1fd1b6ade12373b9e0768c319422e820eb325bd
Martin <ilhammartin2@gmail.com>
2024-01-08 13:05:07 +0700

    kasus sederhana

---
+++ kasus_sederhana/index.js
@@ -0,0 +1,25 @@
+const readline = require('node:readline');
+
+const { stdin: input, stdout: output } = require('node:process');
+
+const pilih = readline.createInterface({input,output})
+
+
+
+const inputData = ()=>{
+    pilih.question("Masukan Nama : ",(nama) => {
+        pilih.question("masukan Umur : ", (umur)=>{
+            console.log( `nama anda ${nama} umur anda ${umur}`)
+        pilih.close()
+        })
+    })
+   
+}
+
+pilih.question("1.Input Data \n 2. Delete Data 3.Cari Data",(pilihan) => {
+    if(pilihan == "1") {
+        inputData()
+        console.log("data berhasil ditambah")
+        
+    }
+})
