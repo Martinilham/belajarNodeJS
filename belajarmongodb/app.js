3ca33e175815b4ad44e815bb90e6fd42d6fd3448
Martin <ilhammartin2@gmail.com>
2024-01-12 21:05:58 +0700

    konek databases
    masih eror

--- belajarmongodb/app.js
+++ belajarmongodb/app.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://martinilham15:2Azct32sNLJqNgj2@myserver.aw1tteh.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "myfirstdata"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,11 +17,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("myfirstdata").collection('databarang')
+    .insertOne({
+        nama:"martin",
+        email:"martin@gmail.com"
+    },(err,client)=>{
+        if(err) {
+            return console.log("eror")
+        }
+        return console.log("berhasil ditambah")
+    })
+    
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.close(console.log("database connect"));
   }
 }
 run().catch(console.dir);
