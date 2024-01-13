dc6c5820258c0331b52361f0947d1a01baa5a14d
Martin <ilhammartin2@gmail.com>
2024-01-14 01:40:31 +0700

    input data dengan mongoose
    error

--- mongodb-contactapp/util/db.js
+++ mongodb-contactapp/util/db.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose')
+const api = 'mongodb+srv://martinilham15:2Azct32sNLJqNgj2@myserver.aw1tteh.mongodb.net/myfirstdata?retryWrites=true&w=majority';
+
+
+mongoose.connect(api);
+
+const data = mongoose.model('databarang',{
+    nama: String,
+    email: String
+})
+
+const databaru = new data({
+    nama:'martinus',
+    email:"martinus@gmail.com"
+})
+
+databaru.save().then((res)=>console.log(res))
